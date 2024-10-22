9a389ad3cd09e243467424fd92088b2423044c37
dennis-chilas <33022645+dennis-chilas@users.noreply.github.com>
2024-10-22 21:51:59 +0200

    Update script.js - hot reload on save

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function showToast() {
         toast.className = "toast hide";
     }, 1000); 
 }
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log(e)
+    fetch('/ws-port')
+    .then(response => response.json())
+    .then(data => {
+      const wsPort = data.wsPort;
+      const ws = new WebSocket(`ws://localhost:${wsPort}`);
+
+      ws.onopen = () => {
+        console.log('Connected to WebSocket server');
+      };
+
+      ws.onmessage = (event) => {
+        console.log(`Received message: ${event.data}`);
+        if(event.data == "reload")
+            window.location.reload()
+      };
+
+      ws.onclose = () => {
+        console.log('Disconnected from WebSocket server');
+      };
+    })
+    .catch(error => {
+      console.error('Error fetching WebSocket port:', error);
+    });
+});
